bc3252745759347ee40db72d5682fc29426bf98c
ranjit banda learning <ranjit.brk.learning@gmail.com>
2023-03-05 20:02:41 +0530

    blocking UI synchronous sample

---
+++ 1_Long_Running_Task_Blocks_UI/StockAnalyzer.Core.Ranjit/DataStoreSync.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using StockAnalyzer.Core.Domain;
+
+namespace StockAnalyzer.Core
+{
+    public class DataStoreSync
+    {
+        public Dictionary<string, Company> Companies = new Dictionary<string, Company>();
+        public static Dictionary<string, IEnumerable<StockPrice>> Stocks 
+            = new Dictionary<string, IEnumerable<StockPrice>>();
+
+        private string basePath { get; }
+
+        public DataStoreSync(string basePath)
+        {
+            this.basePath = basePath;
+        }
+
+        public Dictionary<string, IEnumerable<StockPrice>> LoadStocks()
+        {
+            // THE BELOW LINE IS COMMENTED TO SEE TO THAT EVERY TIME THE FILE GETS READ
+            //if (Stocks.Any()) return Stocks;
+
+            var prices = GetStockPrices();
+
+            Stocks = prices
+                .GroupBy(x => x.Ticker)
+                .ToDictionary(x => x.Key, x => x.AsEnumerable());
+
+            return Stocks;
+        }
+
+        private IList<StockPrice> GetStockPrices()
+        {
+            //var lines = File.ReadAllLines(@"StockPrices_Small.csv");
+            var lines = File.ReadAllLines(@"StockPrices_Big.csv");
+
+            var data = new List<StockPrice>();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var segments = line.Split(',');
+
+                for (var i = 0; i < segments.Length; i++) segments[i] = segments[i].Trim('\'', '"');
+                var price = new StockPrice
+                {
+                    Ticker = segments[0],
+                    TradeDate = DateTime.ParseExact(segments[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                    Volume = Convert.ToInt32(segments[6], CultureInfo.InvariantCulture),
+                    Change = Convert.ToDecimal(segments[7], CultureInfo.InvariantCulture),
+                    ChangePercent = Convert.ToDecimal(segments[8], CultureInfo.InvariantCulture),
+                };
+                data.Add(price);
+            }
+
+            return data;
+        }
+    }
+}
---
+++ 1_Long_Running_Task_Blocks_UI/StockAnalyzer.Windows.Ranjit/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using StockAnalyzer.Core;
+using StockAnalyzer.Core.Domain;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace StockAnalyzer.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            #region Before loading stock data
+            var watch = new Stopwatch();
+            watch.Start();
+            StockProgress.Visibility = Visibility.Visible;
+            StockProgress.IsIndeterminate = true;
+
+            Search.Content = "Cancel";
+            #endregion
+
+            //RANJIT NOTE - The following method Load Stock is a heavy computation task and 
+            //  as it is run synchronously it will block the UI thread - You can check by
+            //  by dragging the window after clicking on the SEARCH button
+            DataStoreSync dataStore = new DataStoreSync(Environment.CurrentDirectory);
+            var test = dataStore.LoadStocks();
+            Stocks.ItemsSource = test[Ticker.Text];
+
+            #region After stock data is loaded
+            StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
+            StockProgress.Visibility = Visibility.Hidden;
+            Search.Content = "Search";
+            #endregion
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
